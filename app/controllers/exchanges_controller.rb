0dab428b3e7070df9dbbc77ffbaaa85370466bee
idanceinbetween <46828701+idanceinbetween@users.noreply.github.com>
2019-06-20 00:41:46 +0100

    Create exchange

--- app/controllers/exchanges_controller.rb
+++ app/controllers/exchanges_controller.rb
@@ -16,6 +16,15 @@ class ExchangesController < ApplicationController
         else
             render json: {error: 'Exchange not found.'}, status: 404 
         end 
+    end
+
+    def create
+        exchange = Exchange.new(exchange_params)
+        if exchange.save
+            render json: exchange
+        else 
+            render json: {error: 'Server error.'}, status: 404
+        end 
     end 
 
     def update
@@ -30,6 +39,6 @@ class ExchangesController < ApplicationController
     private
 
     def exchange_params
-        params.require(:exchange).permit(:exchange_stat_id, :date)
+        params.require(:exchange).permit(:exchange_stat_id, :date, :gift_id, :gifter_id, :recipient_id)
     end 
 end
